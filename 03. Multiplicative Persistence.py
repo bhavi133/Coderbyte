ee9fc87191dd357cff80c3ca979ab47fc8d1a43a
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-04-05 21:21:50 +0530

    Create 03. Multiplicative Persistence.py

---
+++ 03. Multiplicative Persistence.py
@@ -0,0 +1,13 @@
+Problem Statement : Have the function MultiplicativePersistence(num) take the num parameter being passed which will always be a positive integer and return its multiplicative persistence which is the number of times you must multiply the digits in num until you reach a single digit. For example: if num is 39 then your program should return 3 because 3 * 9 = 27 then 2 * 7 = 14 and finally 1 * 4 = 4 and you stop at 4.
+
+def MultiplicativePersistence(num):
+    count = 0
+    while num >= 10:
+      count += 1
+      product = 1
+      for i in str(num):
+        product *= int(i)
+      num = product
+    return count
+ 
+print(MultiplicativePersistence(input()))
