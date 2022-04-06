3fb688118bba85e49298ba36399db6b331a6caf1
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-04-06 20:16:57 +0530

    Create 06. Swap Case.py

---
+++ 06. Swap Case.py
@@ -0,0 +1,7 @@
+Problem Statement : Have the function SwapCase(str) take the str parameter and swap the case of each character. For example: if str is "Hello World" the output should be hELLO wORLD. Let numbers and symbols stay the way they are.
+
+def SwapCase(strParam):
+  return strParam.swapcase()
+
+# keep this function call here 
+print(SwapCase("Hello World"))
